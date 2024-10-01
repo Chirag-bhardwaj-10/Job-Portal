14562f74633317204a9eb29e7ffc1c529331c0a9
Chirag-bhardwaj-10 <chiragbhardwaj426@gmail.com>
2024-10-01 18:01:25 +0530

    test2

--- backend/index.js
+++ backend/index.js
@@ -25,8 +25,8 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOptions = {
-    // origin:'http://localhost:5173',
-    origin:'https://job-portal-backend-qx14.onrender.com',
+    origin:'http://localhost:5173',
+    //origin:'https://job-portal-backend-qx14.onrender.com',
     credentials:true
 }
 app.use(cors(corsOptions));
